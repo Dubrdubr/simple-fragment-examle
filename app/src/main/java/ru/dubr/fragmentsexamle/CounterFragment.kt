833da098a5d6903c33f78f3c095a1bb525b1f0fd
Dubravin Evgeny <dubrdubr23@gmail.com>
2022-03-01 21:43:42 +0300

    add fragment layout

---
+++ app/src/main/java/ru/dubr/fragmentsexamle/CounterFragment.kt
@@ -0,0 +1,25 @@
+package ru.dubr.fragmentsexamle
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.dubr.fragmentsexamle.databinding.FragmentCounterBinding
+
+class CounterFragment: Fragment() {
+
+    private lateinit var binding: FragmentCounterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCounterBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+}
