96ce8093a2d9fccf9fb056a7276ba34e9d086060
Dubravin Evgeny <dubrdubr23@gmail.com>
2022-03-02 00:15:51 +0300

    add fragment with counter & quote

--- app/src/main/java/ru/dubr/fragmentsexamle/CounterFragment.kt
+++ app/src/main/java/ru/dubr/fragmentsexamle/CounterFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.dubr.fragmentsexamle.databinding.FragmentCounterBinding
 
-class CounterFragment: Fragment() {
+class CounterFragment : Fragment() {
 
     private lateinit var binding: FragmentCounterBinding
 
@@ -18,8 +18,48 @@ class CounterFragment: Fragment() {
     ): View? {
         binding = FragmentCounterBinding.inflate(inflater, container, false)
 
+        binding.counterTextView.text = getString(R.string.screen_label, getCounterValue())
+        binding.quoteTextView.text = getQuote()
 
+        binding.launchNextButton.setOnClickListener { onLaunchNextPressed() }
+        binding.goBackButton.setOnClickListener { onBackPressed() }
 
         return binding.root
+    }
+
+    private fun onLaunchNextPressed() {
+        val fragment = newInstance(
+            counterValue = (requireActivity() as MainActivity).getScreensCount() + 1,
+            quote = (requireActivity() as MainActivity).createQuote()
+        )
+        parentFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
+    private fun onBackPressed() {
+        requireActivity().onBackPressed()
+    }
+
+    private fun getCounterValue() = requireArguments().getInt(ARG_COUNTER_VALUE)
+    private fun getQuote() = requireArguments().getString(ARG_QUOTE)
+
+    companion object {
+
+        private const val ARG_COUNTER_VALUE = "ARG_COUNTER_VALUE"
+        private const val ARG_QUOTE = "ARG_QUOTE"
+
+        fun newInstance(counterValue: Int, quote: String): CounterFragment {
+            val args = Bundle().apply {
+                putInt(ARG_COUNTER_VALUE, counterValue)
+                putString(ARG_QUOTE, quote)
+            }
+
+            val fragment = CounterFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
--- app/src/main/java/ru/dubr/fragmentsexamle/MainActivity.kt
+++ app/src/main/java/ru/dubr/fragmentsexamle/MainActivity.kt
@@ -1,11 +1,34 @@
 package ru.dubr.fragmentsexamle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.javafaker.Faker
+import ru.dubr.fragmentsexamle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val faker = Faker.instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        if (savedInstanceState == null) {
+            val fragment = CounterFragment.newInstance(
+                counterValue = 1,
+                quote = createQuote()
+            )
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit()
+        }
     }
+
+    fun createQuote(): String = faker.yoda().quote()
+
+    fun getScreensCount() = supportFragmentManager.backStackEntryCount + 1
+
 }
